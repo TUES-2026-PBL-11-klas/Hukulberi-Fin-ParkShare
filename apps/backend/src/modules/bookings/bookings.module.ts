976ad7de6ab511b37902536b817b37ad6d79f14e
DarianGanev <darianganev83@gmail.com>
2026-05-25 19:15:39 +0300

    feat: add UsersModule to BookingsModule imports

--- apps/backend/src/modules/bookings/bookings.module.ts
+++ apps/backend/src/modules/bookings/bookings.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [BookingsController],
   providers: [BookingsService],
 })
